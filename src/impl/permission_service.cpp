f680b3d4d26958293002950ecffb94febddf3454
duanhongyu <duanhongyu@uniontech.com>
2022-08-31 11:40:04 +0800

    fix: 处理dbus唤醒控制中心延时问题
    通过Qpercess唤醒控制中心会导致dccinterface与调用方出现相互调用的现象，需要通过dbusmessage 去调用避免阻塞; 并修改控制中心访问字段
    
    Log: 弹出取消/前往设置弹框后，点击前往设置，调起控制中心时间太长
    Bug: https://pms.uniontech.com/bug-view-154337.html
    Influence: 控制中心-PM-隐私安全
    Change-Id: Ib9cbcba1f962932839b21cf8b4e73f1f31c3a183

--- src/impl/permission_service.cpp
+++ src/impl/permission_service.cpp
@@ -140,13 +140,10 @@ void PermissionService::ShowDisablePermissionDialog(const QString &appId, const 
     }
 
     if (reply.value() == options[1]) {
-        QProcess p;
-        // 还是采用 com.deepin.dde.ControlCenter ，前端 org.deepin.dde.ControlCenter todo ，后续会修改
-        QString showDccCmd = QString("dbus-send --session --print-reply --type=method_call --dest=com.deepin.dde.ControlCenter "
-                                     "/com/deepin/dde/ControlCenter com.deepin.dde.ControlCenter.ShowPage string:privacyAndSecurity/%1").arg(permissionId);
-        p.start(showDccCmd);
-        p.waitForFinished(10000);
-        p.close();
+        QString arg = QString("privacyAndSecurity/%1").arg(permissionId);
+        QDBusMessage message = QDBusMessage::createMethodCall("org.deepin.dde.ControlCenter1", "/org/deepin/dde/ControlCenter1", "org.deepin.dde.ControlCenter1", "ShowPage");
+        message << arg;
+        QDBusConnection::sessionBus().asyncCall(message);
     }
 
     return;
